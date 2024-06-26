18440b896e2f081371e87ae38747482122d01291
Nirmal raj <128925724+nirmalraj002@users.noreply.github.com>
2024-06-26 21:49:14 +0530

    Update stats.py

--- InflexMusic/plugins/tools/stats.py
+++ InflexMusic/plugins/tools/stats.py
@@ -27,7 +27,7 @@ from config import OWNER_ID
 async def stats_global(client, message: Message, _):
     upl = stats_buttons(_, True if message.from_user.id in SUDOERS else False)
     await message.reply_photo(
-        photo=config.STATS_IMG_URL,
+        photo=random.choice(STATS_IMG_URL),
         caption=_["gstats_2"].format(app.mention),
         reply_markup=upl,
     )
@@ -66,12 +66,12 @@ async def overall_stats(client, CallbackQuery, _):
         config.AUTO_LEAVING_ASSISTANT,
         config.DURATION_LIMIT_MIN,
     )
-    med = InputMediaPhoto(media=config.(STATS_IMG_URL), caption=text)
+    med = InputMediaPhoto(media=random.choice(STATS_IMG_URL), caption=text)
     try:
         await CallbackQuery.edit_message_media(media=med, reply_markup=upl)
     except MessageIdInvalid:
         await CallbackQuery.message.reply_photo(
-            photo=config.(STATS_IMG_URL), caption=text, reply_markup=upl
+            photo=random.choice(STATS_IMG_URL), caption=text, reply_markup=upl
         )
 
 
@@ -129,10 +129,10 @@ async def bot_stats(client, CallbackQuery, _):
         call["collections"],
         call["objects"],
     )
-    med = InputMediaPhoto(media=config.(STATS_IMG_URL), caption=text)
+    med = InputMediaPhoto(media=random.choice(STATS_IMG_URL), caption=text)
     try:
         await CallbackQuery.edit_message_media(media=med, reply_markup=upl)
     except MessageIdInvalid:
         await CallbackQuery.message.reply_photo(
-            photo=config.STATS_IMG_URL, caption=text, reply_markup=upl
+            photo=random.choice(STATS_IMG_URL), caption=text, reply_markup=upl
         )
