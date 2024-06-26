b51d7dcb3300a98774170346907fad57ee77fd40
Nirmal raj <128925724+nirmalraj002@users.noreply.github.com>
2024-06-26 14:19:03 +0530

    start.py

--- InflexMusic/plugins/bot/start.py
+++ InflexMusic/plugins/bot/start.py
@@ -35,7 +35,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_photo(
-                photo=random.choice(START_IMG_URL),
+                photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_GROUP),
                 reply_markup=keyboard,
             )
@@ -87,7 +87,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_photo(
-            photo=random.choice(START_IMG_URL),
+            photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -104,7 +104,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        photo=random.choice(START_IMG_URL),
+        photo=config.START_IMG_URL,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -139,7 +139,7 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    photo=random.choice(START_IMG_URL),
+                    photo=config.START_IMG_URL,
                     caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
