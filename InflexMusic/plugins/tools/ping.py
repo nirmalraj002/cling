9eefa288d7c606e102880e8f5c151976af9557ad
Nirmal raj <128925724+nirmalraj002@users.noreply.github.com>
2024-06-26 21:52:05 +0530

    Update ping.py

--- InflexMusic/plugins/tools/ping.py
+++ InflexMusic/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS, PING_IMG_URL
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_photo(
-        photo=config.(PING_IMG_URL),
+        photo=random.choice(PING_IMG_URL),
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await Inflex.ping()
