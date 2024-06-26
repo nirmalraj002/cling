c56f96afea67188d61fea8d39f83353b5c00a4a4
Nirmal raj <128925724+nirmalraj002@users.noreply.github.com>
2024-06-26 20:46:23 +0530

    stop.py

--- InflexMusic/plugins/admins/stop.py
+++ InflexMusic/plugins/admins/stop.py
@@ -18,6 +18,10 @@ async def stop_music(cli, message: Message, _, chat_id):
         return
     await Inflex.stop_stream(chat_id)
     await set_loop(chat_id, 0)
+    photo_url = "https://te.legra.ph/file/5f3347d579c92e984521f.jpg"  # Replace with the URL of the photo you want to send    
+    await message.reply_photo(
+        photo=photo_url          
+    )
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
