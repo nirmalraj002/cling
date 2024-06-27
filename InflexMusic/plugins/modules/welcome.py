f6bd8149d6bd296e0114c6f043827e6588aee579
Nirmal raj <128925724+nirmalraj002@users.noreply.github.com>
2024-06-27 09:13:07 +0530

    Update welcome.py

--- InflexMusic/plugins/modules/welcome.py
+++ InflexMusic/plugins/modules/welcome.py
@@ -59,13 +59,13 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
 
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("VIPMUSIC/assets/wel2.png")
+    background = Image.open("InflexMusic/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor)
     pfp = pfp.resize((892, 880))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype("VIPMUSIC/assets/font.ttf", size=95)
-    welcome_font = ImageFont.truetype("VIPMUSIC/assets/font.ttf", size=45)
+    font = ImageFont.truetype("InflexMusic/assets/fontwel1.ttf", size=95)
+    welcome_font = ImageFont.truetype("InflexMusic/assets/fontwel1.ttf", size=45)
 
     # Draw user's name with shining red fill and dark saffron border
     draw.text((1770, 1015), f": {user}", fill=(255, 0, 0), font=font)
@@ -196,7 +196,7 @@ async def greet_new_member(
                 user.photo.big_file_id, file_name=f"pp{user.id}.png"
             )
         except AttributeError:
-            pic = "VIPMUSIC/assets/upic.png"
+            pic = "InflexMusic/assets/upic.png"
         if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
             try:
                 await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
@@ -209,7 +209,7 @@ async def greet_new_member(
             button_text = "๏ ᴠɪᴇᴡ ɴᴇᴡ ᴍᴇᴍʙᴇʀ ๏"
             add_button_text = "๏ ᴋɪᴅɴᴀᴘ ᴍᴇ ๏"
             deep_link = f"{user.id}"
-            add_link = f"https://t.me/TG_VC_BOT?startgroup=true"
+            add_link = f"https://t.me/sarahmuisic_bot?startgroup=true"
             temp.MELCOW[f"welcome-{member.chat.id}"] = await client.send_photo(
                 member.chat.id,
                 photo=welcomeimg,
